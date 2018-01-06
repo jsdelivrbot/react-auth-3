1540468a2ee6bfb712e7edba72f9a0195dd5cfe8
Joshua Brokaw <joshuabrokaw@gmail.com>
2018-01-06 09:04:15 -0600

    copying of template

---
+++ src/components/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = function() {
+  return <div>Upgraded</div>;
+};
+
+export default App;
---
+++ test/components/app_test.js
@@ -0,0 +1,16 @@
+/*  eslint-env mocha  */
+
+import { renderComponent, expect } from '../test_helper';
+import App from '../../src/components/app';
+
+describe('App', () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
+
+  it('renders something', () => {
+    expect(component).to.exist();
+  });
+});
